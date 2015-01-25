7b09098aa24ad8faf921105b74982c67708ed6b4
jmcota <jmcota@ontica.org>
2015-01-25 18:30:08 -0500

    UserControls as WebUserControl + Command type for http request commands

---
+++ Presentation.Web/RootTypes/Command.cs
@@ -0,0 +1,109 @@
+﻿/* Empiria Presentation Framework 2015 ***********************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Presentation Framework                   System   : Web Presentation Framework        *
+*  Namespace : Empiria.Presentation.Web                         Assembly : Empiria.Presentation.Web.dll      *
+*  Type      : WebUserControl                                   Pattern  : None                              *
+*  Version   : 6.0        Date: 04/Jan/2015                     License  : Please read license.txt file      *
+*                                                                                                            *
+*  Summary   : Represents a command in the context of an http request.                                       *
+*                                                                                                            *
+********************************* Copyright (c) 2002-2015. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Collections.Specialized;
+
+using Empiria.Security;
+
+namespace Empiria.Presentation.Web {
+
+  /// <summary>Represents a command in the context of an http request.</summary>
+  public class Command {
+
+    #region Constructors and parsers
+
+    public Command(HttpRequest request) {
+      Assertion.AssertObject(request, "request");
+
+      this.Request = request;
+    }
+
+    #endregion Constructors and parsers
+
+    #region Public properties
+
+    public string Name {
+      get {
+        if (!String.IsNullOrEmpty(this.Request.Form["hdnEmpiriaPageCommandName"])) {
+          return this.Request.Form["hdnEmpiriaPageCommandName"];
+        } else {
+          return "Undefined";
+        }
+      }
+    }
+
+    private NameValueCollection _commandParameters = null;
+    public NameValueCollection Parameters {
+      get {
+        if (_commandParameters == null) {
+          if (!String.IsNullOrEmpty(Request.Form["hdnEmpiriaPageCommandArguments"])) {
+            _commandParameters = EmpiriaString.ParseQueryString(Request.Form["hdnEmpiriaPageCommandArguments"]);
+          } else {
+            _commandParameters = new NameValueCollection();
+          }
+        }
+        return this._commandParameters;
+      }
+    }
+
+    public HttpRequest Request {
+      get;
+      private set;
+    }
+
+    public EmpiriaUser User {
+      get {
+        return Empiria.Security.EmpiriaUser.Current;
+      }
+    }
+
+    #endregion Public properties
+
+    #region Public methods
+
+    public T GetParameter<T>(string parameterName) {
+      string value = String.IsNullOrEmpty(this.Parameters[parameterName]) ?
+                                        String.Empty : this.Parameters[parameterName];
+      try {
+        if (!String.IsNullOrEmpty(value)) {
+          return (T) Convert.ChangeType(value, typeof(T));
+        } else {
+          throw new WebPresentationException(WebPresentationException.Msg.NullCommandParameter,
+                                             this.Name, parameterName);
+        }
+      } catch (Exception e) {
+        throw new WebPresentationException(WebPresentationException.Msg.CommandParameterError, e,
+                                           this.Name, parameterName, value);
+      }
+    }
+
+    public T GetParameter<T>(string parameterName, T defaultValue) {
+      string value = String.IsNullOrEmpty(this.Parameters[parameterName]) ?
+                                          String.Empty : this.Parameters[parameterName];
+      try {
+        if (!String.IsNullOrEmpty(value)) {
+          return (T) Convert.ChangeType(value, typeof(T));
+        } else {
+          return defaultValue;
+        }
+      } catch (Exception e) {
+        throw new WebPresentationException(WebPresentationException.Msg.CommandParameterError, e,
+                                           this.Name, parameterName, value);
+      }
+    }
+
+    #endregion Public methods
+
+  } // class Command
+
+} // namespace Empiria.Presentation.Web
---
+++ Presentation.Web/RootTypes/WebUserControl.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Presentation Framework 2015 ***********************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Presentation Framework                   System   : Web Presentation Framework        *
+*  Namespace : Empiria.Presentation.Web                         Assembly : Empiria.Presentation.Web.dll      *
+*  Type      : WebUserControl                                   Pattern  : None                              *
+*  Version   : 6.0        Date: 04/Jan/2015                     License  : Please read license.txt file      *
+*                                                                                                            *
+*  Summary   : Abstract type that represents an ASP user control (ascx). All Empiria user controls           *
+*              must be derived from this type.                                                               *
+*                                                                                                            *
+********************************* Copyright (c) 2002-2015. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using System;
+using System.Web.UI;
+using System.Collections.Specialized;
+
+using Empiria.Security;
+
+namespace Empiria.Presentation.Web {
+
+  /// <summary>Abstract type that represents an ASP user control (ascx). All Empiria user controls
+  ///must be derived from this type.</summary>
+  public abstract class WebUserControl : UserControl {
+
+    #region Public properties
+
+    public EmpiriaUser User {
+      get {
+        return Empiria.Security.EmpiriaUser.Current;
+      }
+    }
+
+    #endregion Public properties
+
+    #region Public methods
+
+    public Command GetCurrentCommand() {
+      return new Command(this.Request);
+    }
+
+    #endregion Public methods
+
+  } // class WebUserControl
+
+} // namespace Empiria.Presentation.Web
