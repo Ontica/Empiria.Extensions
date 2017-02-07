27785019923ccd8d5421551b376a61e421b1122f
jmcota <jmcota@ontica.org>
2017-02-07 00:06:44 -0500

    Empiria Microservices initial project

---
+++ Microservices/Empiria.Microservices.AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿/* Empiria Extensions Framework ******************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Extensions Framework                     System   : Empiria Microservices             *
+*  Assembly  : Empiria.Microservices.dll                        Pattern  : Assembly Attributes File          *
+*  Version   : 2.1                                              License  : Please read license.txt file      *
+*																																																						 *
+*  Summary   : Empiria Core Microservices Collection.                                                        *
+*																																																						 *
+********************************** Copyright(c) 1999-2016. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTitle("Empiria.Microservices")]
+[assembly: AssemblyDescription("Empiria Core Microservices Collection.")]
+[assembly: AssemblyVersion("1.0.0308.0")]
+[assembly: AssemblyFileVersion("1.0.0308")]
+[assembly: AssemblyCompany("Ontica")]
+[assembly: AssemblyProduct("Empiria Microservices")]
+[assembly: AssemblyCopyright("(c) 2016-2017. La Vía Óntica SC, Ontica LLC and contributors.")]
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
---
+++ Microservices/README.md
@@ -0,0 +1,11 @@
+﻿Empiria Microservices License
+=============================
+
+Empiria Microservices provides the following RESTful-based microservices:
+
+- Authentication services
+- Caching services
+- ID Generation services
+- Logging services
+- Ontology services
+- Security services
