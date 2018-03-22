a054aa0d2569bcde09c3c890db807e469d3a062a
jmcota <jmcota@ontica.org>
2018-03-22 19:14:00 -0400

    Load ServiceDirectory data directly from the DB, not using a WS call

--- WebApi.Client/RootTypes/ServiceDirectory.cs
+++ WebApi.Client/RootTypes/ServiceDirectory.cs
@@ -12,6 +12,8 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 
+using Empiria.Security;
+
 namespace Empiria.WebApi.Client {
 
   /// <summary>Singleton that holds a list of web services that can be searched
@@ -20,14 +22,7 @@ namespace Empiria.WebApi.Client {
 
     #region Fields
 
-    private Dictionary<string, ServiceHandler> services =
-                                          new Dictionary<string, ServiceHandler>();
-
-    private static readonly string DEFAULT_SERVER_UID =
-                                          ConfigurationData.GetString("Default.WebApiServer");
-
-    private static readonly string SERVICE_DIRECTORY_PATH =
-                                          ConfigurationData.GetString("ServiceDirectory.Path");
+    private Dictionary<string, ServiceHandler> services = new Dictionary<string, ServiceHandler>();
 
     #endregion Fields
 
@@ -37,15 +32,17 @@ namespace Empiria.WebApi.Client {
       this.LoadServices();
     }
 
+
     private static ServiceDirectory _instance = null;
-    private static object syncRoot = new Object();
+    private static object _syncRoot = new Object();
     static public ServiceDirectory Instance {
       get {
         if (_instance == null) {
-          //lock (syncRoot) {
-          //  if (_instance == null)
+          lock (_syncRoot) {
+            if (_instance == null) {
               _instance = new ServiceDirectory();
-          //}
+            }
+          }  // lock
         }
         return _instance;
       }
@@ -62,14 +59,18 @@ namespace Empiria.WebApi.Client {
 
       if (UtilityMethods.HasUriPathFormat(serviceUID)) {
         searchKey = method.Method + " " + UtilityMethods.RemoveDataScopeFromPath(serviceUID);
+
       } else {
         searchKey = UtilityMethods.RemoveDataScopeFromPath(serviceUID);
+
       }
 
       if (services.ContainsKey(searchKey)) {
         return services[searchKey];
+
       } else {
         throw new WebApiClientException(WebApiClientException.Msg.UndefinedServiceUIDOrEndpoint, searchKey);
+
       }
     }
 
@@ -78,16 +79,20 @@ namespace Empiria.WebApi.Client {
     #region Private methods
 
     private void LoadServices() {
-      var httpClient = new HttpApiClient(DEFAULT_SERVER_UID);
+      var clientApplication = ClientApplication.ParseActive(ExecutionServer.ApplicationKey);
 
-      var task = httpClient.GetAsync<ResponseModel<ServiceHandler[]>>(SERVICE_DIRECTORY_PATH);
-
-      task.Wait();
+      var endpointsList = EndpointConfig.GetList(clientApplication);
 
       this.services.Clear();
-      foreach (var item in task.Result.Data) {
-        this.services.Add(item.UID, item);
-        this.services.Add(item.Method + " " + item.Path, item);
+
+      foreach (var endpoint in endpointsList) {
+        var serviceHandler = new ServiceHandler(endpoint);
+
+        this.services.Add(serviceHandler.EndPoint.UID,
+                          serviceHandler);
+
+        this.services.Add($"{serviceHandler.EndPoint.Method} {serviceHandler.EndPoint.Path}",
+                          serviceHandler);
       }
     }
 
