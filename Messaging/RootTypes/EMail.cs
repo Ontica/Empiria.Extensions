5a7ac8f98cb1cfbfa7658f3892409a93a0b2f3a1
jmcota <jmcota@ontica.org>
2018-08-25 15:35:10 -0500

    Rfx EMail.Send() using EMailContent and SendAync() added

--- Messaging/RootTypes/EMail.cs
+++ Messaging/RootTypes/EMail.cs
@@ -1,46 +1,112 @@
-﻿/* Empiria Foundation Framework ******************************************************************************
+﻿/* Empiria Extensions Framework ******************************************************************************
 *                                                                                                            *
-*  Solution  : Empiria Foundation Framework                     System   : Messaging Services                *
-*  Namespace : Empiria.Messaging                                Assembly : Empiria.Messaging.dll             *
-*  Type      : EMail                                            Pattern  : Empiria Semiabstract Object Type  *
-*  Version   : 6.8                                              License  : Please read license.txt file      *
+*  Module   : Messaging Services                           Component : EMail Services                        *
+*  Assembly : Empiria.Messaging.dll                        Pattern   : Service Provider                      *
+*  Type     : EMail                                        License   : Please read LICENSE.txt file          *
 *                                                                                                            *
-*  Summary   : Static methods for send e-mails.                                                              *
+*  Summary  : Provides email delivery services.                                                              *
 *                                                                                                            *
-********************************* Copyright (c) 2002-2017. La Vía Óntica SC, Ontica LLC and contributors.  **/
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
+
 using System.IO;
 using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
+
+using Empiria.StateEnums;
 
 namespace Empiria.Messaging {
 
-  /// <summary>Static methods for send e-mails.</summary>
+  /// <summary>Provides email delivery services.</summary>
   static public class EMail {
 
     #region Public methods
 
-    static public void Send(string eMail, string subject, string body,
+    static public bool IsValidAddress(string address) {
+      try {
+        var addr = new MailAddress(address);
+
+        return addr.Address == address;
+
+      } catch {
+        return false;
+      }
+    }
+
+
+    static public void Send(SendTo sendTo, EMailContent content) {
+      Assertion.AssertObject(sendTo, "sendTo");
+      Assertion.AssertObject(content, "content");
+
+      MailMessage message = EMail.GetMailMessage(sendTo, content);
+
+      try {
+
+        using (SmtpClient smtp = EMail.GetSmptClient()) {
+          smtp.Send(message);
+        }
+
+      } finally {
+        DisposeMessage(message);
+      }
+    }
+
+
+    static public async Task SendAsync(SendTo sendTo, EMailContent content) {
+      Assertion.AssertObject(sendTo, "sendTo");
+      Assertion.AssertObject(content, "content");
+
+      MailMessage message = EMail.GetMailMessage(sendTo, content);
+
+      Action<Task> disposeMessage = (task) => task = Task.Run(() => DisposeMessage(message));
+
+      using (SmtpClient smtp = EMail.GetSmptClient()) {
+        await smtp.SendMailAsync(message)
+                  .ContinueWith(disposeMessage);
+      }
+
+    }
+
+
+    [Obsolete("Use EMail.Send(string, EMailContent) method instead.")]
+    static public void Send(string address, string subject, string body,
                             bool isBodyHtml = false, bool highPriority = false,
                             FileInfo[] attachments = null) {
-      Assertion.AssertObject(eMail, "eMail");
+      Assertion.AssertObject(address, "address");
       Assertion.AssertObject(subject, "subject");
       Assertion.AssertObject(body, "body");
+      attachments = attachments ?? new FileInfo[0];
 
-      MailMessage message = EMail.GetMailMessage(eMail, subject, body,
-                                                 isBodyHtml, highPriority);
-      if (attachments != null) {
-        foreach (var file in attachments) {
-          message.Attachments.Add(new Attachment(file.FullName));
-        }
+      var content = new EMailContent(subject, body, isBodyHtml);
+
+      foreach (var file in attachments) {
+        content.AddAttachment(file);
       }
-      SmtpClient smtp = EMail.GetSmptClient();
-      smtp.Send(message);
+
+      var sendTo = new SendTo(address);
+
+      if (highPriority) {
+        sendTo.SetPriority(Priority.High);
+      }
+
+      Send(sendTo, content);
     }
+
 
     #endregion Public methods
 
     #region Private methods
+
+
+    static private void DisposeMessage(MailMessage message) {
+      foreach (var attachment in message.Attachments) {
+        attachment.Dispose();
+      }
+      message.Attachments.Dispose();
+      message.Dispose();
+    }
+
 
     static private EMailConfig _eMailConfig = null;
     static private EMailConfig GetDefaultEMailConfig() {
@@ -50,37 +116,33 @@ namespace Empiria.Messaging {
       return _eMailConfig;
     }
 
-    static private MailMessage GetMailMessage(string eMail, string subject, string body,
-                                              bool isBodyHtml, bool highPriority) {
+
+    static private MailMessage GetMailMessage(SendTo sendTo, EMailContent content) {
       EMailConfig eMailConfig = EMail.GetDefaultEMailConfig();
 
       var message = new MailMessage();
 
       message.From = new MailAddress(eMailConfig.SenderEMailAddress, eMailConfig.SenderName);
 
-      // converts any semicolon to comma and removes spaces
-      if (eMail.Contains(";")) {
-        eMail = eMail.Replace(";", ",");
-      }
-      eMail = eMail.Replace(" ", String.Empty);
-
-      if (eMail.Contains(",")) {
-        message.To.Add(eMail);  // Multiple addresses separated by commas
-      } else {
-        message.To.Add(new MailAddress(eMail));
-      }
+      SetMessageReceiverAddresses(message, sendTo.Address);
+      SetMessagePriority(message, sendTo.Priority);
 
       if (eMailConfig.BccMirrorEMailAddress.Length != 0) {
         message.Bcc.Add(new MailAddress(eMailConfig.BccMirrorEMailAddress));
       }
-      message.Subject = subject;
-      message.Body = body;
-      message.IsBodyHtml = isBodyHtml;
-      if (highPriority) {
-        message.Priority = MailPriority.High;
+
+      message.Subject = content.Subject;
+      message.Body = content.Body;
+      message.IsBodyHtml = content.IsBodyHtml;
+      message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+      foreach (var file in content.Attachments) {
+        message.Attachments.Add(new Attachment(file.FullName));
       }
+
       return message;
     }
+
 
     static private SmtpClient GetSmptClient() {
       EMailConfig eMailConfig = EMail.GetDefaultEMailConfig();
@@ -90,11 +152,41 @@ namespace Empiria.Messaging {
       smtp.EnableSsl = eMailConfig.EnableSsl;
       smtp.UseDefaultCredentials = eMailConfig.UseDefaultCredentials;
       smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
       if (!smtp.UseDefaultCredentials) {
         smtp.Credentials = new NetworkCredential(eMailConfig.SenderEMailAddress,
                                                  eMailConfig.SenderEMailPassword);
       }
+
       return smtp;
+    }
+
+
+    static private void SetMessagePriority(MailMessage message, Priority priority) {
+      if (priority == Priority.High) {
+        message.Priority = MailPriority.High;
+
+      } else if (priority == Priority.Low) {
+        message.Priority = MailPriority.Low;
+
+      } else {
+        message.Priority = MailPriority.Normal;
+      }
+    }
+
+
+    static private void SetMessageReceiverAddresses(MailMessage message, string address) {
+      // Converts any semicolon to comma and removes spaces
+      if (address.Contains(";")) {
+        address = address.Replace(";", ",");
+      }
+      address = address.Replace(" ", String.Empty);
+
+      if (address.Contains(",")) {
+        message.To.Add(address);  // Multiple addresses separated by commas
+      } else {
+        message.To.Add(new MailAddress(address));
+      }
     }
 
     #endregion Private methods
---
+++ Messaging/RootTypes/EMailContent.cs
@@ -0,0 +1,86 @@
+﻿/* Empiria Extensions Framework ******************************************************************************
+*                                                                                                            *
+*  Module   : Messaging Services                           Component : EMail Services                        *
+*  Assembly : Empiria.Messaging.dll                        Pattern   : Information Holder                    *
+*  Type     : EMailContent                                 License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Holds the contents of an email message.                                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.IO;
+
+using System.Collections.Generic;
+
+namespace Empiria.Messaging {
+
+  /// <summary>Holds the contents of an email message.</summary>
+  public class EMailContent {
+
+    #region Fields
+
+    List<FileInfo> _attachments = new List<FileInfo>();
+
+    #endregion Fields
+
+    #region Constructors and parsers
+
+
+    public EMailContent(string subject, string body, bool isBodyHtml = false) {
+      Assertion.AssertObject(subject, "subject");
+      Assertion.AssertObject(body, "body");
+
+      this.Subject = subject;
+      this.Body = body;
+      this.IsBodyHtml = isBodyHtml;
+    }
+
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public string Subject {
+      get;
+    }
+
+
+    public string Body {
+      get;
+    }
+
+
+    public bool IsBodyHtml {
+      get;
+    }
+
+
+    public FixedList<FileInfo> Attachments {
+      get {
+        return this._attachments.ToFixedList();
+      }
+    }
+
+    #endregion Properties
+
+    #region Methods
+
+    public void AddAttachment(FileInfo fileInfo) {
+      Assertion.AssertObject(fileInfo, "fileInfo");
+      Assertion.Assert(fileInfo.Exists, $"The file {fileInfo.FullName} does not exist.");
+
+      this._attachments.Add(fileInfo);
+    }
+
+
+    public bool RemoveAttachment(FileInfo fileInfo) {
+      Assertion.AssertObject(fileInfo, "fileInfo");
+
+      return this._attachments.Remove(fileInfo);
+    }
+
+    #endregion Methods
+
+  }  // class EMailContent
+
+}  // namespace Empiria.Messaging
