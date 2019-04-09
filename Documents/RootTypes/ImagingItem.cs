4a8ecb23fd61aa3e65afc609bcce8537bdf647ea
jmcota <jmcota@ontica.org>
2019-04-08 23:43:40 -0400

    Add new fields to ImagingItem: DigitalizationDate, DigitalizedBy, Status

--- Documents/RootTypes/ImagingItem.cs
+++ Documents/RootTypes/ImagingItem.cs
@@ -1,19 +1,21 @@
 ﻿/* Empiria Extensions Framework ******************************************************************************
 *                                                                                                            *
-*  Solution  : Empiria Extensions Framework                   System   : Empiria I/O Services                *
-*  Namespace : Empiria.IO                                     Assembly : Empiria.IO.dll                      *
-*  Type      : ImagingItem                                    Pattern  : Empiria Object Type                 *
-*  Version   : 6.8                                            License  : Please read license.txt file        *
+*  Module   : Documentation Services                       Component : Document Management                   *
+*  Assembly : Empiria.Documents.dll                        Pattern   : Abstract Domain Type                  *
+*  Type     : ImagingItem                                  License   : Please read LICENSE.txt file          *
 *                                                                                                            *
-*  Summary   : Abstract class that represents an imaging folder or an image file.                            *
+*  Summary  : Abstract class that represents an imaging folder or a file (a real image or document).         *
 *                                                                                                            *
-********************************* Copyright (c) 2009-2017. La Vía Óntica SC, Ontica LLC and contributors.  **/
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
+
+using Empiria.Contacts;
 using Empiria.Json;
+using Empiria.StateEnums;
 
 namespace Empiria.Documents {
 
-  /// <summary>Abstract class that represents an imaging folder or an image file.</summary>
+  /// <summary>Abstract class that represents an imaging folder or a file (a real image or document)</summary>
   abstract public class ImagingItem : BaseObject {
 
     #region Constructors and parsers
@@ -22,9 +24,11 @@ namespace Empiria.Documents {
       // Required by Empiria Framework.
     }
 
+
     static public ImagingItem Parse(int id) {
       return BaseObject.ParseId<ImagingItem>(id);
     }
+
 
     static public ImagingItem Empty {
       get {
@@ -32,9 +36,11 @@ namespace Empiria.Documents {
       }
     }
 
+
     #endregion Constructors and parsers
 
     #region Public properties
+
 
     [DataField("FilesCount")]
     public int FilesCount {
@@ -42,14 +48,36 @@ namespace Empiria.Documents {
       protected set;
     }
 
+
     [DataField("ImagingItemPath")]
     public string ItemPath {
       get;
       protected set;
     }
 
+
+    [DataField("DigitalizationDate")]
+    public DateTime DigitalizationDate {
+      get;
+      protected set;
+    } = ExecutionServer.DateMinValue;
+
+
+    [DataField("DigitalizedById")]
+    public Contact DigitalizedBy {
+      get;
+      protected set;
+    }
+
+
     [DataField("ImagingItemExtData")]
     public JsonObject ImagingItemExtData {
+      get;
+      protected set;
+    }
+
+    [DataField("ImagingItemStatus", Default = EntityStatus.Active)]
+    public EntityStatus Status {
       get;
       protected set;
     }
