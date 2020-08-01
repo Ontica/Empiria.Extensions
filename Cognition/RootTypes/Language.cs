28c05ff789d49b560b21adda37a5a243eb75e9ff
José Manuel Cota <jmcota@ontica.org>
2020-08-01 15:59:27 -0400

    TextTranslator.Translate(text, language) service added

---
+++ Cognition/RootTypes/Language.cs
@@ -0,0 +1,23 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Cognitive Services                         Component : Service provider                        *
+*  Assembly : Empiria.Cognition.ESign.dll                Pattern   : Enumerated Type                         *
+*  Type     : Language                                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Describes a text translation language.                                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Cognition {
+
+  /// <summary>Describes a text translation language.</summary>
+  public enum Language {
+
+    English,
+
+    Spanish
+
+  }
+
+}
---
+++ Cognition/RootTypes/TextTranslator.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Cognitive Services                         Component : Service provider                        *
+*  Assembly : Empiria.Cognition.dll                      Pattern   : Service provider                        *
+*  Type     : TextTranslator                             License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides text translation services.                                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Cognition {
+
+  /// <summary>Provides text translation services.</summary>
+  static public class TextTranslator {
+
+    #region Public services
+
+
+    static public string Translate(string text, Language translateTo = Language.English) {
+      if (String.IsNullOrWhiteSpace(text)) {
+        return text;
+      }
+      if (translateTo != Language.English) {
+        new NotImplementedException($"TranslateTo language has not been implemented {translateTo.ToString()}.");
+      }
+
+      return TranslateText(text, translateTo);
+    }
+
+
+    #endregion Public services
+
+    #region Private methods
+
+    static private string TranslateText(string text, Language translateTo) {
+      throw new NotImplementedException("TranslateText");
+    }
+
+    #endregion Private methods
+
+  } // class TextTranslator
+
+}
