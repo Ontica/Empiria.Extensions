e3c779ebb0437745e9ff9c1d0e665dae0572d4e3
José Manuel Cota <jmcota@ontica.org>
2020-08-01 14:06:44 -0400

    Empiria Cognitive services assembly added

---
+++ Cognition/Empiria.Cognition.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Cognitive Services                         Component : Service provider                        *
+*  Assembly : Empiria.Cognition.dll                      Pattern   : Assembly Attributes File                *
+*  Type     : None                                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides cognitive services as text translation and text recognition.                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ Cognition/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria Cognitive Services
+
+Provides cognitive services as text translation and text recognition.
+
+# License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+# Copyright
+
+Copyright © 2020-2020. La Vía Óntica SC, Ontica LLC and colaborators.
