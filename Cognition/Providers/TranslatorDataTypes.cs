c2b1d8df6f0cafcecc43274791d3303ee8c332f5
cyanez <chris.yanez@gmail.com>
2020-08-04 14:37:18 -0500

    Translation services provider added

---
+++ Cognition/Providers/TranslatorDataTypes.cs
@@ -0,0 +1,97 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Cognitive Services                         Component : Service provider                        *
+*  Assembly : Empiria.Cognition.dll                      Pattern   : Data Transfer Objects                   *
+*  Type     : TranslatorDataTypes                        License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Define data types for Microsoft Azure Cognition Translator Services.                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Cognition.Providers {
+  /// <summary>Summary  : Define data types for Microsoft Azure Cognition Translator Services.</summary>
+
+  internal class Alignment {
+    internal string Proj {
+      get; set;
+    }
+
+  }
+
+
+  internal class SentenceLength {
+    internal int[] SrcSentLen {
+      get; set;
+    }
+
+    internal int[] TransSentLen {
+      get; set;
+    }
+
+  }
+
+
+  internal class Translation {
+    internal string Text {
+      get; set;
+    }
+
+    internal TextResult Transliteration {
+      get; set;
+    }
+
+    internal string To {
+      get; set;
+    }
+
+    internal Alignment Alignment {
+      get; set;
+    }
+    internal SentenceLength SentLen {
+      get; set;
+    }
+
+  }
+
+
+  internal class DetectedLanguage {
+    internal string Language {
+      get; set;
+    }
+
+    internal float Score {
+      get; set;
+    }
+
+  }
+
+
+  internal class TextResult {
+    internal string Text {
+      get; set;
+    }
+
+    internal string Script {
+      get; set;
+    }
+
+  }
+
+
+  internal class TranslatorDataTypes {
+    internal DetectedLanguage DetectedLanguage {
+      get; set;
+    }
+
+    internal TextResult SourceText {
+      get; set;
+    }
+
+    internal Translation[] Translations {
+      get; set;
+    }
+
+  }
+
+}
---
+++ Cognition/Providers/TranslatorProvider.cs
@@ -0,0 +1,73 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Cognitive Services                         Component : Service provider                        *
+*  Assembly : Empiria.Cognition.dll                      Pattern   : Service provider                        *
+*  Type     : TranslatorProvider                         License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides text translation services usign Microsoft Azure Cognition Services.                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using Newtonsoft.Json;
+
+namespace Empiria.Cognition.Providers {
+
+  /// <summary>Provides text translation services usign Microsoft Azure Cognition Services.</summary>
+  public class TranslatorProvider {
+
+    private string subscriptionKey = string.Empty;
+    private const string endpoint = "https://api.cognitive.microsofttranslator.com/";
+
+    public TranslatorProvider(string translationToken) {
+      subscriptionKey = translationToken;
+    }
+
+    #region Public Methods
+
+    public async Task<string> TranslateToText(string text, string toLanguaje) {
+      try {
+        string translation = await TranslateToJson(text, toLanguaje);
+
+        TranslatorDataTypes[] deserializedOutput = JsonConvert.DeserializeObject<TranslatorDataTypes[]>(translation);
+        TranslatorDataTypes result = deserializedOutput[0];
+
+        return result.Translations[0].Text;
+      } catch (Exception ex) {
+        throw new Exception("Sorry I can't translate this text: " + text + ". ", ex);
+      }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private async Task<string> TranslateToJson(string text, string toLanguage) {
+      string uri = endpoint + "/translate?api-version=3.0&to=" + toLanguage;
+
+      object[] body = new object[] { new { Text = text } };
+      var requestBody = JsonConvert.SerializeObject(body);
+
+      using (var client = new HttpClient()) {
+        using (var request = new HttpRequestMessage()) {
+          request.Method = HttpMethod.Post;
+          request.RequestUri = new Uri(uri);
+          request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+          request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+          HttpResponseMessage response = await client.SendAsync(request);
+          var responseBody = await response.Content.ReadAsStringAsync();
+          dynamic result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseBody), Formatting.Indented);
+
+          return result;
+        }
+      }  // using
+    }
+
+    #endregion
+
+  }  // class Translator
+}
