fb221dcdd7dd73be880faacb91df715b7cf920fe
José Manuel Cota <jmcota@ontica.org>
2020-09-08 14:22:21 -0400

    EventNotifier service call added (not yet connected to messaging)

---
+++ Messaging/Services/EventNotifier.cs
@@ -0,0 +1,77 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Messaging                          Component : Services Layer                          *
+*  Assembly : Empiria.Messaging.dll                      Pattern   : Service provider                        *
+*  Type     : EventNotifier                              License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Service to notify or announce that an event was occured.                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Json;
+
+namespace Empiria.Messaging {
+
+  /// <summary>Service to notify or announce that an event was occured.</summary>
+  static public class EventNotifier {
+
+    #region Public services
+
+    static public void Notify(string eventName) {
+      NotifyImplementation(eventName);
+    }
+
+
+    static public void Notify(Enum eventName) {
+      NotifyImplementation(eventName.ToString());
+    }
+
+
+    static public void Notify(string eventName, JsonObject payload) {
+      NotifyImplementation(eventName, payload);
+    }
+
+
+    static public void Notify(string eventName, object payload) {
+      NotifyImplementation(eventName, JsonObject.Parse(payload));
+    }
+
+
+    static public void Notify(Enum eventName, JsonObject payload) {
+      NotifyImplementation(eventName.ToString(), payload);
+    }
+
+
+    static public void Notify(Enum eventName, object payload) {
+      NotifyImplementation(eventName.ToString(), JsonObject.Parse(payload));
+    }
+
+    #endregion Public services
+
+    #region Private methods
+
+    static private void NotifyImplementation(string eventName) {
+      Assertion.AssertObject(eventName, "eventName");
+
+      //var message = new EventMessage(eventName);
+
+      //MessageBroker.SendMessage(message);
+    }
+
+
+    static private void NotifyImplementation(string eventName, JsonObject payload) {
+      Assertion.AssertObject(eventName, "eventName");
+      Assertion.AssertObject(payload, "payload");
+
+      //var message = new EventMessage(eventName, payload);
+
+      //MessageBroker.SendMessage(message);
+    }
+
+
+    #endregion Private methods
+
+  }  // class EventNotifier
+
+} // namespace Empiria.Messaging
