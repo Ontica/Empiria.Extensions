9bea7a3aef1bbe2c069faf50120dbc99772c60cf
José Manuel Cota <jmcota@ontica.org>
2021-01-31 14:31:56 -0500

    Rfx Authentication logic moved to use cases (login services ver 3.0)

---
+++ WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,109 @@
+﻿/* Empiria Extensions Framework ******************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Web Api                              Component : Web Api                               *
+*  Assembly : Empiria.WebApi.dll                           Pattern   : Controller                            *
+*  Type     : AuthenticationController                     License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web api methods for user authentication.                                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.ServiceModel.Channels;
+using System.Web;
+using System.Web.Http;
+
+using Empiria.Security;
+
+using Empiria.Services.Authentication;
+
+namespace Empiria.WebApi.Controllers {
+
+  /// <summary>Web api methods for user authentication.</summary>
+  public class AuthenticationController : WebApiController {
+
+    #region Web Apis
+
+    [HttpPost, AllowAnonymous]
+    [Route("v3/security/login-token")]
+    public SingleObjectModel GetLoginToken([FromBody] AuthenticationFields fields) {
+      fields = PrepareAuthenticationFields(fields);
+
+      using (var usecases = AuthenticationUseCases.UseCaseInteractor()) {
+        var token = usecases.GenerateAuthenticationToken(fields);
+
+        return new SingleObjectModel(base.Request, token);
+      }
+    }
+
+
+    [HttpPost, AllowAnonymous]
+    [Route("v3/security/login")]
+    public SingleObjectModel Login([FromBody] AuthenticationFields fields) {
+      fields = PrepareAuthenticationFields(fields);
+
+      using (var usecases = AuthenticationUseCases.UseCaseInteractor()) {
+        EmpiriaPrincipal principal = usecases.Authenticate(fields);
+
+        AuthenticationHttpModule.SetPrincipal(principal);
+
+        return new SingleObjectModel(base.Request, MapToOAuthResponse(principal),
+                                     "Empiria.Security.OAuthObject");
+      }
+    }
+
+    #endregion Web Apis
+
+    #region Helper methods
+
+    private string GetApplicationKeyFromHeader() {
+      return this.GetRequestHeader<string>("ApplicationKey");
+    }
+
+    private string GetClientIpAddress() {
+      var request = this.Request;
+
+      if (request.Properties.ContainsKey("MS_HttpContext")) {
+        return ((HttpContextWrapper) request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+
+      } else if (request.Properties.ContainsKey(RemoteEndpointMessageProperty.Name)) {
+        var prop = (RemoteEndpointMessageProperty) request.Properties[RemoteEndpointMessageProperty.Name];
+
+        return prop.Address;
+
+      } else if (HttpContext.Current != null) {
+        return HttpContext.Current.Request.UserHostAddress;
+
+      } else {
+        return String.Empty;
+      }
+    }
+
+    static public object MapToOAuthResponse(EmpiriaPrincipal principal) {
+      return new {
+        access_token = principal.Session.Token,
+        token_type = principal.Session.TokenType,
+        expires_in = principal.Session.ExpiresIn,
+        refresh_token = principal.Session.RefreshToken,
+        user = new {
+          uid = principal.Identity.User.Id,
+          username = principal.Identity.User.UserName,
+          email = principal.Identity.User.EMail
+        }
+      };
+    }
+
+    private AuthenticationFields PrepareAuthenticationFields(AuthenticationFields fields) {
+      base.RequireHeader("User-Agent");
+      base.RequireBody(fields);
+
+      fields.AppKey = GetApplicationKeyFromHeader();
+      fields.IpAddress = GetClientIpAddress();
+
+      return fields;
+    }
+
+    #endregion Helper methods
+
+  }  // class LoginController
+
+}  // namespace Empiria.WebApi.Controllers
