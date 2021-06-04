2b2dc1d8363091b93a1466aab2ca371a04dee664
José Manuel Cota <jmcota@ontica.org>
2021-06-04 14:23:35 -0500

    Call ExecutionServer.Preload() method in Application_Start()

--- WebApi/RootTypes/WebApiApplication.cs
+++ WebApi/RootTypes/WebApiApplication.cs
@@ -67,6 +67,8 @@ namespace Empiria.WebApi {
     protected override void Application_Start(object sender, EventArgs e) {
       base.Application_Start(sender, e);
 
+      ExecutionServer.Preload();
+
       Register();
     }
 
