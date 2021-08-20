94b175f0d5f9df33d21d581ebbbfbba05f0dad1d
José Manuel Cota <jmcota@ontica.org>
2021-08-20 11:09:43 -0400

    Comment pdf fields reader/writer to remove itext7/CastleCrypto reference

--- Office/Providers/PdfFieldsWriter.cs
+++ Office/Providers/PdfFieldsWriter.cs
@@ -1,68 +1,68 @@
-﻿/* Empiria Extensions ****************************************************************************************
-*                                                                                                            *
-*  Module   : Office Integration Services                Component : PDF service provider                    *
-*  Assembly : Empiria.Office.dll                         Pattern   : Provider                                *
-*  Type     : PdfFieldsWriter                            License   : Please read LICENSE.txt file            *
-*                                                                                                            *
-*  Summary  : Writes fields values inside of a PDF form.                                                     *
-*                                                                                                            *
-************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
+﻿///* Empiria Extensions ****************************************************************************************
+//*                                                                                                            *
+//*  Module   : Office Integration Services                Component : PDF service provider                    *
+//*  Assembly : Empiria.Office.dll                         Pattern   : Provider                                *
+//*  Type     : PdfFieldsWriter                            License   : Please read LICENSE.txt file            *
+//*                                                                                                            *
+//*  Summary  : Writes fields values inside of a PDF form.                                                     *
+//*                                                                                                            *
+//************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.IO;
 
-using iText.Forms;
-using iText.Kernel.Pdf;
+//using iText.Forms;
+//using iText.Kernel.Pdf;
 
-namespace Empiria.Office.Providers {
+//namespace Empiria.Office.Providers {
 
-  /// <summary>Writes fields values inside of a PDF form.</summary>
-  public class PdfFieldsWriter {
+//  /// <summary>Writes fields values inside of a PDF form.</summary>
+//  public class PdfFieldsWriter {
 
-    private PdfAcroForm form;
-
-
-    private PdfFieldsWriter(PdfDocument document) {
-      this.form = PdfAcroForm.GetAcroForm(document, false);
-    }
+//    private PdfAcroForm form;
 
 
-    static public MemoryStream WriteFields(Stream stream, IEnumerable<PdfFieldDTO> newFields) {
-      Assertion.AssertObject(stream, "stream");
-      Assertion.AssertObject(newFields, "newFields");
-
-      var outputStream = new MemoryStream();
-
-      using (PdfDocument document = new PdfDocument(new PdfReader(stream), new PdfWriter(outputStream))) {
-        var writer = new PdfFieldsWriter(document);
-
-        foreach (var newField in newFields.ToList()) {
-          writer.SetFieldValue(newField.Key, newField.Value);
-        }
-      }
-
-      return outputStream;
-    }
+//    private PdfFieldsWriter(PdfDocument document) {
+//      this.form = PdfAcroForm.GetAcroForm(document, false);
+//    }
 
 
-    #region Private Methods
+//    static public MemoryStream WriteFields(Stream stream, IEnumerable<PdfFieldDTO> newFields) {
+//      Assertion.AssertObject(stream, "stream");
+//      Assertion.AssertObject(newFields, "newFields");
 
-    private bool ExistsField(string key) {
-      return form.GetFormFields().ContainsKey(key);
-    }
+//      var outputStream = new MemoryStream();
+
+//      using (PdfDocument document = new PdfDocument(new PdfReader(stream), new PdfWriter(outputStream))) {
+//        var writer = new PdfFieldsWriter(document);
+
+//        foreach (var newField in newFields.ToList()) {
+//          writer.SetFieldValue(newField.Key, newField.Value);
+//        }
+//      }
+
+//      return outputStream;
+//    }
 
 
-    private void SetFieldValue(string key, string value) {
-      if (this.ExistsField(key)) {
-        this.form.GetField(key).SetValue(value);
-      } else {
-        throw new Exception("The field with key: " + key + " does not exist.");
-      }
-    }
+//    #region Private Methods
 
-    #endregion Private Methods
+//    private bool ExistsField(string key) {
+//      return form.GetFormFields().ContainsKey(key);
+//    }
 
-  }  // class PdfFieldsWriter
 
-}  // namespace Empiria.Office.Providers
+//    private void SetFieldValue(string key, string value) {
+//      if (this.ExistsField(key)) {
+//        this.form.GetField(key).SetValue(value);
+//      } else {
+//        throw new Exception("The field with key: " + key + " does not exist.");
+//      }
+//    }
+
+//    #endregion Private Methods
+
+//  }  // class PdfFieldsWriter
+
+//}  // namespace Empiria.Office.Providers
