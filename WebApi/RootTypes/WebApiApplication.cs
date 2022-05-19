ac5b767ad1bb5c11f0a0aeb913d1642b7335df5a
José Manuel Cota <jmcota@ontica.org>
2022-05-18 23:07:12 -0400

    Rfx Json settings were moved from WebApiApplication to JsonConverter

--- WebApi/RootTypes/WebApiApplication.cs
+++ WebApi/RootTypes/WebApiApplication.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/* Empiria Extensions Framework ******************************************************************************
+*                                                                                                            *
+*  Module   : Web Api Core Services                     Component : Core Types                               *
+*  Assembly : Empiria.WebApi.dll                        Pattern   : WebApiGlobal Type                        *
+*  Type     : WebApiApplication                         License   : Please read LICENSE.txt file             *
+*                                                                                                            *
+*  Summary  : Empiria HttpApplication object for Http WebApi services                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.ExceptionHandling;
@@ -6,6 +16,60 @@ using System.Web.Http.ExceptionHandling;
 using Empiria.Json;
 
 namespace Empiria.WebApi {
+
+  /// <summary>Empiria HttpApplication object for Http WebApi services.</summary>
+  public class WebApiApplication : WebApiGlobal {
+
+    static public void Register() {
+      RegisterGlobalHandlers(GlobalConfiguration.Configuration);
+
+      GlobalConfiguration.Configure(WebApiConfig.RegisterCallback);
+
+      RegisterFormatters(GlobalConfiguration.Configuration);
+
+      RegisterGlobalFilters(GlobalConfiguration.Configuration);
+
+    }
+
+
+    protected override void Application_Start(object sender, EventArgs e) {
+      base.Application_Start(sender, e);
+
+      ExecutionServer.Preload();
+
+      Register();
+    }
+
+
+    static private void RegisterFormatters(HttpConfiguration config) {
+      Newtonsoft.Json.JsonSerializerSettings settings = JsonConverter.JsonSerializerDefaultSettings();
+
+      config.Formatters.JsonFormatter.SerializerSettings = settings;
+      config.Formatters.Remove(config.Formatters.XmlFormatter);         // Remove Xml formatter
+    }
+
+
+    static private void RegisterGlobalHandlers(HttpConfiguration config) {
+
+      config.MessageHandlers.Add(new AuditTrailHandler());
+
+      config.Services.Replace(typeof(IExceptionHandler), new WebApiExceptionHandler());
+
+      config.MessageHandlers.Add(new WebApiResponseHandler());
+
+      // config.MessageHandlers.Add(new StorageContextHandler());
+    }
+
+
+    static private void RegisterGlobalFilters(HttpConfiguration config) {
+      // Denies anonymous access to every controller without the AllowAnonymous attribute
+      if (!ExecutionServer.IsPassThroughServer) {
+        config.Filters.Add(new AuthorizeAttribute());
+      }
+    }
+
+  }  // class WebApiApplication
+
 
   static internal class WebApiConfig {
 
@@ -37,88 +101,18 @@ namespace Empiria.WebApi {
         name: "Error404Handler",
         routeTemplate: "{*url}",
         defaults: new {
-          controller = "Security", action = "Http404ErrorHandler"
+          controller = "Security",
+          action = "Http404ErrorHandler"
         }
       );
     }
 
     static private void RegisterWebApiRoutes(HttpConfiguration config) {
-      WebApiConfig.RegisterHttp404ErrorHandlerRoute(config.Routes);
+      RegisterHttp404ErrorHandlerRoute(config.Routes);
     }
 
     #endregion Private methods
 
   }  // class WebApiConfig
-
-  public class WebApiApplication : WebApiGlobal {
-
-    static public void Register() {
-      RegisterGlobalHandlers(GlobalConfiguration.Configuration);
-
-      GlobalConfiguration.Configure(WebApiConfig.RegisterCallback);
-
-      RegisterFormatters(GlobalConfiguration.Configuration);
-
-      RegisterGlobalFilters(GlobalConfiguration.Configuration);
-
-    }
-
-
-    protected override void Application_Start(object sender, EventArgs e) {
-      base.Application_Start(sender, e);
-
-      ExecutionServer.Preload();
-
-      Register();
-    }
-
-
-    static private void RegisterFormatters(HttpConfiguration config) {
-      var settings = new Newtonsoft.Json.JsonSerializerSettings();
-
-      settings.Formatting = Newtonsoft.Json.Formatting.Indented;
-
-      settings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-
-      settings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
-
-
-      // Empiria Json converters
-
-      settings.DateFormatString = "yyyy'-'MM'-'dd'T'HH':'mm':'ss";
-      settings.Converters.Add(new DateTimeConverter());
-      settings.Converters.Add(new ValueObjectConverter());
-      settings.Converters.Add(new DataViewConverter());
-      settings.Converters.Add(new DataRowConverter());
-
-      // Third party converters
-      settings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
-
-      config.Formatters.JsonFormatter.SerializerSettings = settings;
-      config.Formatters.Remove(config.Formatters.XmlFormatter);         // Remove Xml formatter
-    }
-
-
-    static private void RegisterGlobalHandlers(HttpConfiguration config) {
-
-      config.MessageHandlers.Add(new AuditTrailHandler());
-
-      config.Services.Replace(typeof(IExceptionHandler), new WebApiExceptionHandler());
-
-      config.MessageHandlers.Add(new WebApiResponseHandler());
-
-      // config.MessageHandlers.Add(new StorageContextHandler());
-
-    }
-
-
-    static private void RegisterGlobalFilters(HttpConfiguration config) {
-      // Denies anonymous access to every controller without the AllowAnonymous attribute
-      if (!ExecutionServer.IsPassThroughServer) {
-        config.Filters.Add(new AuthorizeAttribute());
-      }
-    }
-
-  }  // class WebApiApplication
 
 }  // namespace Empiria.WebApi
