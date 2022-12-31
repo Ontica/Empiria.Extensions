7e0bc3eb1c5985678eb2d095c7ca95dbf67250f5
José Manuel Cota <jmcota@ontica.org>
2022-12-31 11:34:19 -0500

    LibrariesRegistry and BaseFunctionLibrary types were added

---
+++ Expressions/Libraries/BaseFunctionsLibrary.cs
@@ -0,0 +1,57 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Expressions                        Component : Libraries                               *
+*  Assembly : Empiria.Extensions.dll                     Pattern   : Abstract class                          *
+*  Type     : BaseFunctionsLibrary                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Abstract class used to implement Empiria Expressions functions libraries.                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Collections.Generic;
+
+namespace Empiria.Expressions {
+
+  /// <summary>Abstract class used to implement Empiria Expressions functions libraries.</summary>
+  public abstract class BaseFunctionsLibrary {
+
+    private readonly List<Function> _functions = new List<Function>();
+
+
+    public IList<Function> Functions {
+      get {
+        return _functions;
+      }
+    }
+
+    protected void Add(Function function) {
+      Assertion.Require(function, nameof(function));
+
+      _functions.Add(function);
+    }
+
+
+    internal void AddRange(IEnumerable<Function> functions) {
+      Assertion.Require(functions, nameof(functions));
+
+      _functions.AddRange(functions);
+    }
+
+
+    internal Function GetFunction(IToken token) {
+      var function = _functions.Find(x => x.Lexeme == token.Lexeme);
+
+      Assertion.Require(function, "function");
+
+      return function;
+    }
+
+
+    internal bool HasFunction(IToken token) {
+      return _functions.Exists(x => x.Lexeme == token.Lexeme);
+    }
+
+
+  }  // abstract class FunctionsLibrary
+
+}  // namespace Empiria.Expressions
---
+++ Expressions/Libraries/LibrariesRegistry.cs
@@ -0,0 +1,83 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Expressions                        Component : Expressions Parsing                     *
+*  Assembly : Empiria.Extensions.dll                     Pattern   : Information Holder                      *
+*  Type     : LibrariesRegistry                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Mantains a registry of loaded function libraries to be parsed and invoked.                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Collections.Generic;
+
+namespace Empiria.Expressions.Libraries {
+
+  /// <summary>Mantains a registry of loaded function libraries that can be parsed and invoked.</summary>
+  internal class LibrariesRegistry {
+
+    private readonly List<BaseFunctionsLibrary> _libraries = new List<BaseFunctionsLibrary>();
+
+    internal LibrariesRegistry() {
+      // no-op
+    }
+
+    internal void Add(BaseFunctionsLibrary library) {
+      Assertion.Require(library, nameof(library));
+
+      _libraries.Add(library);
+    }
+
+
+    internal Function GetFunction(IToken token) {
+      foreach (var library in _libraries) {
+        if (library.HasFunction(token)) {
+          return library.GetFunction(token);
+        }
+      }
+
+      throw Assertion.EnsureNoReachThisCode($"Unregistered library function '{token.Lexeme}'.");
+    }
+
+
+    internal FunctionHandler GetFunctionHandler(IToken token,
+                                                List<IToken> parameters,
+                                                IDictionary<string, object> data) {
+      Func<FunctionHandler> creator = GetFunction(token).Calle;
+
+      FunctionHandler functionHandler = creator.Invoke();
+
+      functionHandler.SetParameters(parameters);
+      functionHandler.LoadData(data);
+
+      return functionHandler;
+    }
+
+
+    internal bool HasRegisteredFunction(string candidate) {
+      IList<Function> allFunctions = GetAllFunctions();
+
+      foreach (var @function in allFunctions) {
+        if (candidate == function.Lexeme) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    #region Helpers
+
+    private IList<Function> GetAllFunctions() {
+      var allFunctions = new List<Function>(64);
+
+      foreach (var library in _libraries) {
+        allFunctions.AddRange(library.Functions);
+      }
+      return allFunctions;
+    }
+
+    #endregion Helpers
+
+  }  // class LibrariesRegistry
+
+}  // namespace Empiria.Expressions
