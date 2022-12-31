1d3e5aefb1fc84fd112f73897c3c6f694e6f8249
José Manuel Cota <jmcota@ontica.org>
2022-12-31 14:33:53 -0500

    Derive Function type from Token adding function's arity and its calle

--- Expressions/Parsing/Function.cs
+++ Expressions/Parsing/Function.cs
@@ -4,16 +4,35 @@
 *  Assembly : Empiria.Extensions.dll                     Pattern   : Service provider                        *
 *  Type     : Function                                   License   : Please read LICENSE.txt file            *
 *                                                                                                            *
-*  Summary  : Represents a user defined function or a library function in an expression.                     *
+*  Summary  : ERxpression token that represents a function that can be evaluated using a calle.              *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
 
 namespace Empiria.Expressions {
 
-  /// <summary>Represents a user defined function or a library function in an expression.</summary>
-  internal class Function {
+  /// <summary>Expression token that represents a function that can be evaluated using a calle.</summary>
+  public class Function : Token {
 
+    public Function(string lexeme, int arity,
+                    Func<FunctionHandler> calle) : base(TokenType.Function, lexeme) {
+
+      Assertion.Require(arity >= 0, "Function arity must be non negative");
+      Assertion.Require(calle, nameof(calle));
+
+      Arity = arity;
+      Calle = calle;
+    }
+
+
+    public int Arity {
+      get;
+    }
+
+
+    public Func<FunctionHandler> Calle {
+      get;
+    }
 
   }  // class Function
 
