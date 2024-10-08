10d4829b1f8130fb523f4809cb7425ec74de9de4
José Manuel Cota <jmcota@ontica.org>
2024-10-08 09:28:43 -0600

    IWorkItemProvider interface and WorkflowProviders factory were added

---
+++ Services/Providers/IWorkItemProvider.cs
@@ -0,0 +1,20 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Services Framework                         Component : External Services Providers             *
+*  Assembly : Empiria.Services.dll                       Pattern   : Dependency inversion interface          *
+*  Type     : IWorkItemProvider                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Interface used to integrate Empiria.Services with workflow systems using work items.           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Services.Providers {
+
+  /// <summary>Interface used to integrate Empiria.Services with workflow systems using work items.</summary>
+  public interface IWorkItemProvider {
+
+    void SendEvent(IWorkItemEvent workItemEvent);
+
+  }  // interface IWorkItemProvider
+
+}  // namespace Empiria.Services.Providers
---
+++ Services/Providers/WorkflowProviders.cs
@@ -0,0 +1,28 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Services Framework                         Component : External Services Providers             *
+*  Assembly : Empiria.Services.dll                       Pattern   : Providers Factory                       *
+*  Type     : WorkflowProviders                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Factory that provides object instances used to access workflow services.                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Reflection;
+
+namespace Empiria.Services.Providers {
+
+  /// <summary>Factory that provides object instances used to access workflow services.</summary>
+  static internal class WorkflowProviders {
+
+    static internal IWorkItemProvider WorkItemProvider() {
+      Type type = ObjectFactory.GetType("Empiria.Workflow",
+                                        "Empiria.Workflow.Services.WorkflowStepsProvider");
+
+      return (IWorkItemProvider) ObjectFactory.CreateObject(type);
+    }
+
+  }  // class WorkflowProviders
+
+}  // namespace Empiria.Services.Providers
