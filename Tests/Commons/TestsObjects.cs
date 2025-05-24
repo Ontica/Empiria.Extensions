5f40fbec8447b424e12c9eb72a8c345bef866969
José Manuel Cota <jmcota@ontica.org>
2025-05-24 14:14:50 -0400

    TestsObjects service provider added to Empiria.Tests

---
+++ Tests/Commons/TestsObjects.cs
@@ -0,0 +1,31 @@
+﻿/* Empiria Extensions ****************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Tests                              Component : Infrastructure provider                 *
+*  Assembly : Empiria.Tests.dll                          Pattern   : Service provider                        *
+*  Type     : TestsObjects                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides services to get Empiria BaseObject instances used for tests.                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Tests {
+
+  /// <summary>Provides services to get Empiria BaseObject instances used for tests.</summary>
+  static public class TestsObjects {
+
+    static public T TryGetObject<T>() where T: BaseObject {
+      var objects = BaseObject.GetFullList<T>()
+                              .FindAll(x => !x.IsEmptyInstance);
+
+      if (objects.Count == 0) {
+        return null;
+      }
+
+      int randomIndex = EmpiriaMath.GetRandom(0, objects.Count - 1);
+
+      return objects[randomIndex];
+    }
+
+  }  // class TestsObjects
+
+}  // namespace Empiria.Tests
