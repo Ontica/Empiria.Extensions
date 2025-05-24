a42a678ce2fec11a7ad6b24eb0f057133fbaeb60
José Manuel Cota <jmcota@ontica.org>
2025-05-24 17:45:12 -0400

    TestsObjects.TryGetObject<T>(Predicate<T> match) method added

--- Tests/Commons/TestsObjects.cs
+++ Tests/Commons/TestsObjects.cs
@@ -8,6 +8,8 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
+using System;
+
 namespace Empiria.Tests {
 
   /// <summary>Provides services to get Empiria BaseObject instances used for tests.</summary>
@@ -16,6 +18,20 @@ namespace Empiria.Tests {
     static public T TryGetObject<T>() where T: BaseObject {
       var objects = BaseObject.GetFullList<T>()
                               .FindAll(x => !x.IsEmptyInstance);
+
+      if (objects.Count == 0) {
+        return null;
+      }
+
+      int randomIndex = EmpiriaMath.GetRandom(0, objects.Count - 1);
+
+      return objects[randomIndex];
+    }
+
+
+    static public T TryGetObject<T>(Predicate<T> match) where T : BaseObject {
+      var objects = BaseObject.GetFullList<T>()
+                              .FindAll(match);
 
       if (objects.Count == 0) {
         return null;
